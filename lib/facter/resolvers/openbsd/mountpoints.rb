63070f28ae8a5738155fff738daa3bfaac5b4e2d
Klemens Nanni <kn@openbsd.org>
2026-03-01 09:28:21 +0300

    mountpoints: OpenBSD: split around parentheses, use descriptive variables
    The regex is not pretty, but splits the line more clearly into two parts
    to be divided further independently.  Prefer names to indices.

--- lib/facter/resolvers/openbsd/mountpoints.rb
+++ lib/facter/resolvers/openbsd/mountpoints.rb
@@ -27,11 +27,13 @@ module Facter
           end
 
           def add_mount_points_fact(line)
-            (elem, _, options) = line.partition("\s(")
-            elem = elem.split("\s")
-            options = options.chop!.split(",\s")
-            @fact_list[:mountpoints][elem[2]] = { device: elem[0], filesystem: elem[4],
-                                                  options: options }
+            (tokens, options) = line.split(/\s*\(|\)\s*/, 0)
+            (device, _on, path, _type, filesystem) = tokens.split("\s")
+            @fact_list[:mountpoints][path] = {
+              device: device,
+              filesystem: filesystem,
+              options: options.split(",\s")
+            }
           end
 
           def retrieve_sizes_for_mounts
